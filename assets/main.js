7aae92233d0edd4587330cf1959b49659b363a33
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-11-15 00:31:20 +0400

    adding javascript value

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+const from = document.getElementById('from');
+const from_amount = document.getElementById('from_amount');
+const to = document.getElementById('to');
+const to_amount = document.getElementById('to_amount');
+const rate = document.getElementById('rate');
+const swap = document.getElementById('swap');
